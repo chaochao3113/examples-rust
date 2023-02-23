e51922a4ca6df0f5812c18aa57a1b5843455459c
ChrisZzz <2810744497@qq.com>
2023-02-23 22:28:51 +0800

    feat: trait 50%

---
+++ trait-ex/src/bin/overloading.rs
@@ -0,0 +1,34 @@
+use std::ops;
+
+struct Foo;
+struct Bar;
+
+#[derive(Debug)]
+struct FooBar;
+
+#[derive(Debug)]
+struct BarFoo;
+
+impl ops::Add<Bar> for Foo {
+    type Output = FooBar;
+
+    fn add(self, _rhs: Bar) -> Self::Output {
+        println!("> Foo.add(Bar) was called");
+
+        FooBar
+    }
+}
+
+impl ops::Add<Foo> for Bar {
+    type Output = BarFoo;
+    fn add(self, _rhs: Foo) -> Self::Output {
+        println!("> Bar.add(Foo) was called");
+
+        BarFoo
+    }
+}
+
+fn main() {
+    println!("Foo + Bar = {:?}", Foo + Bar);
+    println!("Bar + Foo = {:?}", Bar + Foo);
+}
