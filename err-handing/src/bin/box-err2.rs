4e320eba3ebc0981c92ae55226c0dab0b0aeff58
ChrisZzz <2810744497@qq.com>
2023-02-28 21:18:54 +0800

    feat: err-handing

---
+++ err-handing/src/bin/box-err2.rs
@@ -0,0 +1,60 @@
+use std::{error, num::ParseIntError, fmt};
+
+type Result<T> = std::result::Result<T, DoubleError>;
+
+#[derive(Debug)]
+enum DoubleError {
+    EmptyVec,
+    Parse(ParseIntError),
+}
+
+impl fmt::Display for DoubleError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DoubleError::EmptyVec => 
+                write!(f, "please use a vector with at least one element"),
+            DoubleError::Parse(ref e) => e.fmt(f),
+        }
+    }
+}
+
+impl error::Error for DoubleError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            DoubleError::EmptyVec => None,
+            // 原因采取内部对错误类型的实现。它隐式地转换成了 trait 对象 `&error:Error`。
+            // 这可以工作，因为内部的类型已经实现了 `Error` trait。
+            DoubleError::Parse(ref e) => Some(e),
+        }
+    }
+}
+
+impl From<ParseIntError> for DoubleError {
+    fn from(err: ParseIntError) -> Self {
+        DoubleError::Parse(err)
+    }
+}
+
+fn double_first(vec: Vec<&str>) -> Result<i32> {
+    let first = vec.first().ok_or(DoubleError::EmptyVec)?;
+    let parsed = first.parse::<i32>()?;
+
+    Ok(2 * parsed)
+}
+
+fn print(result: Result<i32>) {
+    match result {
+        Ok(n)  => println!("The first doubled is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    let numbers = vec!["42", "93", "18"];
+    let empty = vec![];
+    let strings = vec!["tofu", "93", "18"];
+
+    print(double_first(numbers));
+    print(double_first(empty));
+    print(double_first(strings));
+}
