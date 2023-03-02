c58ae3e4f61f8a36ca295d0d9e6054c9fb1ffaea
ChrisZzz <2810744497@qq.com>
2023-03-02 22:28:17 +0800

    feat: result

---
+++ std-type/src/bin/why.rs
@@ -0,0 +1,57 @@
+mod checked {
+    #[derive(Debug)]
+    pub enum MathError {
+        DivisionByZero,
+        NegativeLogarithm,
+        NegativeSquareRoot,
+    }
+
+    pub type MathResult = Result<f64, MathError>;
+
+    pub fn div(x: f64, y: f64) -> MathResult {
+        if y == 0.0 {
+            Err(MathError::DivisionByZero)
+        } else {
+            Ok(x / y)
+        }
+    }
+
+    pub fn sqrt(x: f64) -> MathResult {
+        if x < 0.0 {
+            Err(MathError::NegativeSquareRoot)
+        } else {
+            Ok(x.sqrt())
+        }
+    }
+
+    pub fn ln(x: f64) -> MathResult {
+        if x < 0.0 {
+            Err(MathError::NegativeLogarithm)
+        } else {
+            Ok(x.ln())
+        }
+    }
+
+    fn op_(x: f64, y: f64) -> MathResult {
+        let ratio = div(x, y)?;
+
+        let ln = ln(ratio)?;
+
+        sqrt(ln)
+    }
+
+    pub fn op(x: f64, y: f64) {
+        match op_(x, y) {
+            Err(why) => panic!("{}", match why {
+                MathError::NegativeLogarithm => "1",
+                MathError::DivisionByZero => "2",
+                MathError::NegativeSquareRoot => "3",
+            }),
+            Ok(val) => println!("{}", val),
+        }
+    }
+}
+
+fn main() {
+    checked::op(1.0, 10.0)
+}
